19cd9395ac1286849f1ecf37be2e590b866dd714
caleb-long19 <caleblong199@gmail.com>
2021-05-11 22:11:07 +0100

    COMP2007 - The Hunt For The Spittoon Gangs Lost Treasure - Changelog 7
    Additions
    Added Custom Main Character Model
    Added Custom Textures to Main Character Model
    Added Custom Barkeeper Model
    Added Custom Textures to Barkeeper Model
    Added Idle Animation to Main Character
    Added Idle Animation to Barkeeper Model
    Added Running Animation to Main Character Model
    Added Run To Stop Animation to Main Character Model
    Added Custom Textures to Saloon
    Added Saloon Logo
    Added multiple textures/materials to multiple folders
    
    Changes
    Reduced size of Well Object
    
    To Do
    Add gameplay functionality
     - Coin Pickups
     - AI Dialog
     - Enemies (Attack and Chase)
     - Item Pickup
     - Jumping
     - Attack (Hurt Enemies)

--- COMP2007_LostTreasure3D/Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ COMP2007_LostTreasure3D/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -18,7 +18,8 @@ public class PlayerMovement : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if(direction.magnitude >= 0.1f)
+
+        if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothV, turningSmoothTime);
